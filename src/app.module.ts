55470038a309380b031d94cf7bff6395664d1fce
IkkYa <akunikky11@gmail.com>
2025-01-13 21:13:59 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { User, UserSchema } from './users/schema/users.schemas';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://akunikky11:EV4he5UzAXT5EptR@cluster0.kcviq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     ),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     UsersModule,
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import BaseResponse from './utils/response/base.response';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './users/schema/users.schemas';
+import { Model } from 'mongoose';
+import { ResponseSuccess } from './interface/response';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService extends BaseResponse {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {
+    super();
+  }
+
+  async getAllUsers(): Promise<ResponseSuccess> {
+    const users = await this.userModel.find();
+
+    return this._success('Data berhasil ditemukan!', users);
   }
 }
