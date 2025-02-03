356edf7dbf396a814f2bed094c16458caa81ed20
IkkYa <akunikky11@gmail.com>
2025-02-03 20:41:40 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
-import * as serverless from 'serverless-http';
-
-const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-  );
-
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -24,13 +15,8 @@ async function bootstrap() {
       },
     }),
   );
-
-  app.enableCors(); // Pastikan CORS diaktifkan
-
-  await app.init();
+  app.enableCors();
+  await app.listen(process.env.PORT);
+  console.log(`jalan bro di 3040`);
 }
-
 bootstrap();
-
-// Export handler untuk digunakan di serverless
-export const handler = serverless(expressApp);
